4d12f4eeb539c5e2e15037cb1d516e94ad6756c8
Amitesh Ranjan <amiranja4@publicisgroupe.net>
2019-07-02 11:10:56 +0530

    Added styled components to weatherApp

--- weatherApp/withHooks/src/Components/Season/Season.js
+++ weatherApp/withHooks/src/Components/Season/Season.js
@@ -2,13 +2,22 @@
 /* eslint-disable no-useless-constructor */
 import React from 'react';
 import propTypes from 'prop-types';
-import './Season.css'
+import './Season.css';
+import styled from 'styled-components';
+
 // import 'semantic-ui-react';
 
 function Season(props) {
     var season = getSeason(props.lat, new Date().getMonth());
     var text = season === 'Winter' ? 'Hey, its Winter' : 'Its Summer';
     var iconName = season === 'Winter' ? 'snowflake' : 'sun';
+
+    const SeasonName = styled.h1`
+    font-size: 3.5em;
+    text-align: center;
+    color: black;
+`;
+
 
     function getSeason(lat, month) {
         if (month > 2 && month < 9) {
@@ -23,9 +32,9 @@ function Season(props) {
         <div className="getSeason">
             <div className={`season ${season}`}>
                 <i className={`icon-left massive ${iconName} icon`} />
-                <h1>
+                <SeasonName>
                     {text}
-                </h1>
+                </SeasonName>
                 <i className={`icon-right massive ${iconName} icon`} />
             </div>
         </div>
