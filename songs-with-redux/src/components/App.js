d890a0668cdcd7152cd3f8e58abae4cb88d5b4ba
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-07-05 15:02:50 +0530

    Solved accessibility issues with react-axe

--- songs-with-redux/src/components/App.js
+++ songs-with-redux/src/components/App.js
@@ -13,19 +13,23 @@ const App= ()=> {
 //   margin-top : 20px; 
 // `
   return (
-    <Grid container columns={2} stackable>
-      <Grid.Column>
-        <ContainerElement>
-          <SongListComponent />
-        </ContainerElement>
-      </Grid.Column>
-      <Grid.Column>
-        <ContainerElement>
-        {/* <Button>This is a hotpink button.</Button> */}
-          <SongDetailComponent />
-        </ContainerElement>
-      </Grid.Column>
-      
+    <Grid container columns={2} stackable role="main">
+      <Grid.Row>
+        {/* <h1>MOVIES DATABASE</h1> */}
+      </Grid.Row>
+      <Grid.Row>
+        <Grid.Column>
+          <ContainerElement>
+            <SongListComponent />
+          </ContainerElement>
+        </Grid.Column>
+        <Grid.Column>
+          <ContainerElement>
+          {/* <Button>This is a hotpink button.</Button> */}
+            <SongDetailComponent />
+          </ContainerElement>
+        </Grid.Column>
+      </Grid.Row>
     </Grid>
   );
 }
--- songs-with-redux/src/index.js
+++ songs-with-redux/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import axe from 'react-axe';
 
 import App from './components/App';
 import reducers from './reducers';
+
+if (process.env.NODE_ENV !== 'production') {
+    axe(React, ReactDOM, 1000);
+  }
 
 ReactDOM.render(
 <Provider store={createStore(reducers)}>
