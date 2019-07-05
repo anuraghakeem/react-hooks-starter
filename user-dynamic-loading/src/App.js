9c48cc41a324b46a4594493a094f44fd4b7c3ee8
Anurag Hakeem <anuhakee@publicisgroupe.net>
2019-07-05 16:53:02 +0530

    added dynamic loading of components

--- user-dynamic-loading/src/App.js
+++ user-dynamic-loading/src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import Loadable from 'react-loadable';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Modules configured. This can come from a database...
+const available = ['Header', 'Body'];
+class App extends Component {
+  state = { modules: [], active: [] };
+  // Toggle module
+  toggleModule = (nome) => {
+    const { active } = this.state, modules = [];
+    // Add or remove from list
+    let i = active.indexOf(nome);
+    if (i > -1) active.splice(i, 1);
+    else active.push(nome);
+    // Create loadables. THIS IS THE MAGIC!
+    active.map(m => {
+      return modules.push(Loadable({
+        loader: () => import('./components/'+m), // Here can be any component!
+        loading: () => <div>Loading { m }...</div>,
+      }));
+    });
+    this.setState({ ...this.state, modules, active });
+  }
+  render() {
+    const { modules, active } = this.state;
+    return (
+      <div className="App">
+        { modules.map((item, i) => {
+          let Module = modules[i]
+          return <Module key={i} />
+        }) }
+        <ul>
+          { available.map((name, i) => (
+            <li key={i}>
+              { name }
+              <input type="checkbox"
+                checked={active.indexOf(name) > -1}
+                onClick={e => this.toggleModule(name)}
+              />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
-
 export default App;
---
+++ user-dynamic-loading/src/components/Body.js
@@ -0,0 +1,7 @@
+import React from 'react';
+const Body = () => (
+  <p className="App-intro">
+    To get started, edit <code>src/App.js</code> and save to reload.
+  </p>
+)
+export default Body;
